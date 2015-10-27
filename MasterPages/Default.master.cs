6f6b78cf4a3dec04ff50581f4c78f08f36b8efd1
Shrutika Mendhe <shrutika.mendhe@rgensolutions.com>
2015-10-27 11:28:39 +0530

    added script manager

--- MasterPages/Default.master.cs
+++ MasterPages/Default.master.cs
@@ -9,6 +9,16 @@ public partial class MasterPages_Default : System.Web.UI.MasterPage
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-
+        //------Added by Atul
+        if (!Request.Url.AbsoluteUri.Contains("Reports"))
+        {
+            if (ScriptManager.GetCurrent(Page).IsInAsyncPostBack)
+            {
+            }
+            else
+            {
+                //CancelUnexpectedRePost();
+            }
+        }
     }
 }
