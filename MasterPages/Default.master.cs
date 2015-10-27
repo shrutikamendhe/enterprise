4792e095e57778f36b5b30f7eeee203b7b6df720
Shrutika Mendhe <shrutika.mendhe@rgensolutions.com>
2015-10-27 13:16:53 +0530

    add all html

--- MasterPages/Default.master.cs
+++ MasterPages/Default.master.cs
@@ -28,4 +28,16 @@ public partial class MasterPages_Default : System.Web.UI.MasterPage
             Response.Write("<h1> Error: </h1><p>" + ex.Message + "</p><br/><h1>" + ex.StackTrace + "</h1>");
         }
     }
+
+    protected void btnLogout_Click(object sender, EventArgs e)
+    {
+        if (Session != null)
+        {
+            Session.Clear();
+            Session.RemoveAll();
+            Session.Abandon();
+        }
+
+        Response.Redirect("Login.aspx");
+    }
 }
