dcff63c2d54a68c66fc9cb53c39396b371049185
Shrutika Mendhe <shrutika.mendhe@rgensolutions.com>
2015-10-28 15:18:47 +0530

    database check

--- Default.aspx.cs
+++ Default.aspx.cs
@@ -4,11 +4,37 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Configuration;
 
 public partial class _Default : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+        try
+        {
+            string cs = ConfigurationManager.ConnectionStrings["SQLServerDBConnectionString"].ToString();
+            using (SqlConnection con = new SqlConnection(cs))
+            {
+                con.Open();
+
+                SqlDataReader reader = null;
+                SqlCommand cmd = new SqlCommand("SELECT * FROM FirstTable", con);
+
+                reader = cmd.ExecuteReader();
+
+                reader.Read();
+
+                Response.Write(reader["StringText"].ToString());
+
+                con.Close();
+
+            }
+        }
+        catch (Exception ex)
+        {
+            Response.Write(ex.Message);
+        }
         //if (!IsPostBack)
         //{
         //    Response.Redirect("Home.aspx");
