aabd9e50e27ce366790c89647efa9bddc77c6976
Shrutika Mendhe <shrutika.mendhe@rgensolutions.com>
2015-10-28 18:02:03 +0530

    working code

--- Default.aspx
+++ Default.aspx
@@ -1,7 +1,5 @@
 ﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
 
-<%@ Register Assembly="AjaxControlToolkit" Namespace="AjaxControlToolkit" TagPrefix="asp" %>
-
 <!DOCTYPE html>
 
 <html xmlns="http://www.w3.org/1999/xhtml">
@@ -10,10 +8,8 @@
 </head>
 <body>
     <form id="form1" runat="server">
-        <asp:ScriptManager ID="ScriptManager1" runat="server"></asp:ScriptManager>
         <div>
-            Default Page
-       <asp:ComboBox ID="ComboBox1" runat="server"></asp:ComboBox>
+            
         </div>
     </form>
 </body>
--- Default.aspx.cs
+++ Default.aspx.cs
@@ -14,41 +14,45 @@ public partial class _Default : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-        try
+
+        if (!IsPostBack)
         {
-            string cs = ConfigurationManager.ConnectionStrings["SQLServerDBConnectionString"].ToString();
-            using (SqlConnection con = new SqlConnection(cs))
-            {
-                con.Open();
-
-                SqlDataReader reader = null;
-                SqlCommand cmd = new SqlCommand("SELECT * FROM FirstTable", con);
-
-                reader = cmd.ExecuteReader();
-
-                reader.Read();
-
-                Response.Write("Database response: " + reader["StringText"].ToString());
-
-                con.Close();
-
-            }
-
-
-            WebClient proxy = new WebClient();
-            byte[] data = proxy.DownloadData("http://192.168.0.15:9090/RestfullService.svc/dowork");
-            Stream stream = new MemoryStream(data);
-            DataContractJsonSerializer obj = new DataContractJsonSerializer(typeof(string));
-            string result = obj.ReadObject(stream).ToString();
-            Response.Write("<br/> Service response: " + result.ToString());
+            Response.Redirect("Home.aspx");
+            return;
         }
-        catch (Exception ex)
-        {
-            Response.Write(ex.Message);
-        }
-        //if (!IsPostBack)
+
+        //try
         //{
-        //    Response.Redirect("Home.aspx");
+        //    string cs = ConfigurationManager.ConnectionStrings["SQLServerDBConnectionString"].ToString();
+        //    using (SqlConnection con = new SqlConnection(cs))
+        //    {
+        //        con.Open();
+
+        //        SqlDataReader reader = null;
+        //        SqlCommand cmd = new SqlCommand("SELECT * FROM FirstTable", con);
+
+        //        reader = cmd.ExecuteReader();
+
+        //        reader.Read();
+
+        //        Response.Write("Database response: " + reader["StringText"].ToString());
+
+        //        con.Close();
+
+        //    }
+
+
+        //    WebClient proxy = new WebClient();
+        //    byte[] data = proxy.DownloadData("http://192.168.0.15:9090/RestfullService.svc/dowork");
+        //    Stream stream = new MemoryStream(data);
+        //    DataContractJsonSerializer obj = new DataContractJsonSerializer(typeof(string));
+        //    string result = obj.ReadObject(stream).ToString();
+        //    Response.Write("<br/> Service response: " + result.ToString());
         //}
+        //catch (Exception ex)
+        //{
+        //    Response.Write(ex.Message);
+        //}
+
     }
 }
